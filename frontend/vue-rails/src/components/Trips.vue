5d220293723c462c43dec0df2e29ea5d2fd15e13
jake park <jake@parkjake.com>
2023-07-24 15:27:30 -0400

    wip: CORS

--- frontend/vue-rails/src/components/Trips.vue
+++ frontend/vue-rails/src/components/Trips.vue
@@ -1,14 +1,30 @@
 <script setup>
 import WelcomeItem from './WelcomeItem.vue'
 import { onMounted, onBeforeMount } from 'vue'
+import axios from 'axios';
+
 
 onBeforeMount(() => {
   console.log(`the component is onBeforeMount`);
+
+  axios.get('http://localhost:3000')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
   
 })
 
 onMounted(() => {
   console.log(`the component is now mounted.`)
+
 })
 
 
