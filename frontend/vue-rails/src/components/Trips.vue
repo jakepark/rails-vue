a300cd01414ff45437bfcaadd8f1e72ebb0b0546
jake park <jake@parkjake.com>
2023-07-24 16:21:55 -0400

    update title

--- frontend/vue-rails/src/components/Trips.vue
+++ frontend/vue-rails/src/components/Trips.vue
@@ -1,19 +1,39 @@
 <script setup>
-import WelcomeItem from './WelcomeItem.vue'
-import { onMounted, onBeforeMount } from 'vue'
+import { ref, onMounted, onBeforeMount } from 'vue'
 import axios from 'axios';
 
+// const props = defineProps({
+//   loading: false,
+//   trips: null,
+//   error: null,
+// })
+
+// const trips = [
+//   {
+//     assignee: "user_1",
+//     owner: "user_1",
+//   },
+//   {
+//     assignee: "user_1",
+//     owner: "user_1",
+//   }
+// ]
+
+// const trips = await fetch(`http://localhost:3000`)
+//   .then((r) => r.json())
 
 onBeforeMount(() => {
   console.log(`the component is onBeforeMount`);
-
   axios.get('http://localhost:3000')
     .then(function (response) {
-      // handle success
+      // handle
+      console.log(`success!`);
       console.log(response);
+      // props.trips = response.data
     })
     .catch(function (error) {
       // handle error
+      console.error(`error!`);
       console.log(error);
     })
     .finally(function () {
@@ -31,17 +51,31 @@ onMounted(() => {
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #heading>Trips</template>
+  
+  <!-- header row -->
+  <!-- for attribute in tripProp -->
 
+
+  <!-- for trip in UserTrips -->
+  <!-- row data -->
+
+
+  <div class="trip">
+    Trip Review
     
-    <!-- header row -->
-    <!-- for attribute in tripProp -->
+    <div v-if="loading" class="loading">Loading...</div>
 
+    <div v-if="error" class="error">{{ error }}</div>
 
-    <!-- for trip in UserTrips -->
-    <!-- row data -->
+    <div v-if="trips" class="content">
+      Trip Content
 
-  </WelcomeItem>
-
+      {{ trips }}
+      <!-- <div v-for="trip in trips">
+        Trip
+        <p>{{ trip.assignee }}</p>
+        <p>{{ trip.owner }}</p>
+      </div> -->
+    </div>
+  </div>
 </template>
