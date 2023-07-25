d8205bc957f6c8b9835c0aafe18e14f35ba4b216
jake park <jake@parkjake.com>
2023-07-24 21:37:15 -0400

    update main

--- frontend/vue-rails/src/main.js
+++ frontend/vue-rails/src/main.js
@@ -6,9 +6,6 @@ import App from './App.vue'
 const app = createApp(App)
 
 import ModalForm from './components/ModalForm.vue'
-// app.component("modal", {
-//   template: "#modal-template"
-// });
-
 app.component("ModalForm", ModalForm)
+
 app.mount('#app')
